1523c37afd9e492ba9702c0af1068fa446310ac4
johntrammell <johntrammell@fc79c29e-9b4d-0410-b950-8f24507593ec>
2008-05-21 15:49:14 +0000

    rudimentary tests in place
    git-svn-id: https://scheme-tap-writer.googlecode.com/svn/trunk@23 fc79c29e-9b4d-0410-b950-8f24507593ec

--- src/tap/writer.scm
+++ src/tap/writer.scm
@@ -1,10 +1,15 @@
 ;;; $Id$
 
-(define-module (test tap writer)
+(define-module (tap writer)
    #:export (plan diag ok is isnt))
 
 (define planned #f)
 (define index #f)
+
+(define index++
+  (lambda ()
+    (cond ((eq? index #f) (set! index 1))
+          (else (set! index (+ index 1))))))
 
 (define (plan n)
   (cond (planned (error "Multiple calls to (plan)"))
@@ -19,21 +24,20 @@
 (define (diag1 msg)
   (simple-format #t "# ~a~%" msg))
 
+(define ok
+  (lambda (b)
+    (cond ((not (boolean? b)) (error "Arguments to 'ok' must be boolean"))
+          (b (display-ok))
+          (else (display-not-ok)))))
+
 (define (display-ok)
-  (begin (set! index (+ index 1))
+  (begin (index++)
          (simple-format #t "ok ~a~%" index)
          (flush-all-ports)))
 
 (define (display-not-ok)
   (begin (display "not ")
          (display-ok)))
-
-(define (plan n)
-  (cond (max (error "Multiple calls to (plan)"))
-        (else (begin (set! max n)
-                     (set! has-plan #t)
-                     (simple-format #t "1..~a~%" max)
-                     max))))
 
 (define (no-plan)
   (set! has-plan #f))
@@ -45,9 +49,6 @@
                 (simple-format #t "1..~a~%" index)
                 (flush-all-ports)))))
 
-(define (ok bool)
-  (cond (bool (display-ok))
-        (else (display-not-ok))))
 
 (define (is s1 s2)
   (cond ((equal? s1 s2) (display-ok))
--- t/00-plan.scm
+++ t/00-plan.scm
@@ -1,1 +1,5 @@
-(load ... )
+(use-modules (tap writer))
+(plan 2)
+(diag "wango tango")    ; diagnostic output
+(ok #t)                 ; => ok
+(ok #f)                 ; => not ok
